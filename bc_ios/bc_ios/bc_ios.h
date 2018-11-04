7e0e0880bae5d9dbc9644c6b89ea2ee75121ae4c
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-11-04 15:21:17 +0100

    Changes to make "bc" compatible with ios_system

---
+++ bc_ios/bc_ios/bc_ios.h
@@ -0,0 +1,19 @@
+//
+//  bc_ios.h
+//  bc_ios
+//
+//  Created by Nicolas Holzschuch on 04/11/2018.
+//  Copyright © 2018 Nicolas Holzschuch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for bc_ios.
+FOUNDATION_EXPORT double bc_iosVersionNumber;
+
+//! Project version string for bc_ios.
+FOUNDATION_EXPORT const unsigned char bc_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bc_ios/PublicHeader.h>
+
+
