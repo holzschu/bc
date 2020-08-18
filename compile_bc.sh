e3f465eb587c38477765661769a7c1e66967a76a
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-08-18 09:48:13 +0200

    quiet option for zip in xcframework creation

--- compile_bc.sh
+++ compile_bc.sh
@@ -8,8 +8,8 @@ echo "Downloading header file:"
 curl -OL $HHROOT/ios_system/releases/download/$IOS_SYSTEM_VER/ios_error.h 
 
 # First, creat all frameworks for both architectures: 
-xcodebuild -project bc_ios/bc_ios.xcodeproj -alltargets -sdk iphoneos -configuration Release -quiet
-xcodebuild -project bc_ios/bc_ios.xcodeproj -alltargets -sdk iphonesimulator -configuration Release -quiet
+xcodebuild -project bc_ios/bc_ios.xcodeproj -alltargets -sdk iphoneos -arch arm64 -configuration Release -quiet
+xcodebuild -project bc_ios/bc_ios.xcodeproj -alltargets -sdk iphonesimulator -arch x86_64 -configuration Release -quiet
 
 # then, merge them into XCframeworks:
 for framework in bc_ios
@@ -17,7 +17,8 @@ do
    rm -rf $framework.xcframework
    xcodebuild -create-xcframework -framework bc_ios/build/Release-iphoneos/$framework.framework -framework bc_ios/build/Release-iphonesimulator/$framework.framework -output $framework.xcframework
    # while we're at it, let's compute the checksum:
-   zip -r $framework.xcframework.zip $framework.xcframework
+   rm -f $framework.xcframework.zip
+   zip -rq $framework.xcframework.zip $framework.xcframework
    swift package compute-checksum $framework.xcframework.zip
 done
 
