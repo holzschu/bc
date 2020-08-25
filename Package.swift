2d854d89a05af1da0c93049b2700f46c6c6ff55a
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-08-25 17:31:32 +0200

    Updated to Xcode 12b5

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "bc_ios",
             url: "https://github.com/holzschu/bc/releases/download/1.0/bc_ios.xcframework.zip",
-            checksum: "b1696873822a66697ef2cb7bfcf84cad539daf7e7da9f77a91f6759f75c4892b"
+            checksum: "50f78d889b6915aa3793df9823f2bf07697834e81b5245492bd038b118f50ec8"
         )
     ]
 )
