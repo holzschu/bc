8662c32adeb62c4dc5d539481f68ffbf1690807f
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2021-02-02 12:13:06 +0100

    Update build system to xcfs

--- compile_bc.sh
+++ compile_bc.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
 
-IOS_SYSTEM_VER="2.6"
-
-HHROOT="https://github.com/holzschu"
-
-echo "Downloading header file:"
-curl -OL $HHROOT/ios_system/releases/download/$IOS_SYSTEM_VER/ios_error.h 
-
-# First, creat all frameworks for both architectures: 
-xcodebuild -project bc_ios/bc_ios.xcodeproj -alltargets -sdk iphoneos -arch arm64 -configuration Release -quiet
-xcodebuild -project bc_ios/bc_ios.xcodeproj -alltargets -sdk iphonesimulator -arch x86_64 -configuration Release -quiet
-
-# then, merge them into XCframeworks:
-for framework in bc_ios
-do
-   rm -rf $framework.xcframework
-   xcodebuild -create-xcframework -framework bc_ios/build/Release-iphoneos/$framework.framework -framework bc_ios/build/Release-iphonesimulator/$framework.framework -output $framework.xcframework
-   # while we're at it, let's compute the checksum:
-   rm -f $framework.xcframework.zip
-   zip -rq $framework.xcframework.zip $framework.xcframework
-   swift package compute-checksum $framework.xcframework.zip
-done
-
-
-
+swift run --package-path xcfs build
---
+++ xcfs/Sources/build/main.swift
@@ -0,0 +1,52 @@
+// use it from root folder:
+// `swift run --package-path xcfs build`
+
+import Foundation
+import FMake 
+
+OutputLevel.default = .error
+
+enum Config {
+    static let iOSSystemVersion = "2.7"
+    static let iOSSystemRoot    = "https://github.com/holzschu"
+
+    static var iOSSystemErrorHURL: String {
+        "\(iOSSystemRoot)/ios_system/releases/download/\(iOSSystemVersion)/ios_error.h"
+    }
+}
+
+try download(url: Config.iOSSystemErrorHURL)
+
+// TODO: We can add more platforms here
+let platforms: [Platform] = [.iPhoneOS, .iPhoneSimulator]
+
+let schemes = [ "bc_ios" ]
+
+var checksums: [[String]] = []
+
+for scheme in schemes {
+    try xcxcf(
+        dirPath: ".build",
+        project: "bc_ios/bc_ios",
+        scheme: scheme,
+        platforms: platforms.map { ($0, excludedArchs: []) }
+    )
+
+    try cd(".build") {
+        let zip = "\(scheme).xcframework.zip"
+        try sh("zip -r \(zip) \(scheme).xcframework")
+        let chksum = try sha(path: zip)
+        checksums.append([zip, chksum])
+    }
+}
+
+var releaseNotes =
+"""
+Release notes:
+
+\( checksums.markdown(headers: "File", "SHA 256") )
+
+"""
+
+try write(content: releaseNotes, atPath: ".build/release.md")
+
