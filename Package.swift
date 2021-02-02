5d63bef7c526e98871c0937c186960901dbd61e1
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2021-02-02 12:46:56 +0100

    Update Package.swift to refer to CI build

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "bc_ios",
-            url: "https://github.com/holzschu/bc/releases/download/1.0/bc_ios.xcframework.zip",
-            checksum: "535181787b8bb7aad3f009b5568394bc386c81a58a280f679eaeaeca3a3ad389"
+            url: "https://github.com/holzschu/bc/releases/download/v1.0/bc_ios.xcframework.zip",
+            checksum: "e3d72c562f726614e273efb06f6e63ccd23f9e38b14c468cf9febd4302df5fdd"
         )
     ]
 )
